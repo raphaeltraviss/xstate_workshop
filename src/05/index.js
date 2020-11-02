02aed63a49286e1a33459081034fa66c847d793b
Raphael Traviss <raphael@skyleafdesign.com>
2020-11-02 15:10:42 -0600

    Exercise 5 x, dx, and px dragging context

--- src/05/index.js
+++ src/05/index.js
@@ -3,42 +3,65 @@ import { createMachine, assign, interpret } from 'xstate';
 const elBox = document.querySelector('#box');
 const elBody = document.body;
 
+
 const machine = createMachine({
   initial: 'idle',
-  // Set the initial context
-  // Clue: {
-  //   x: 0,
-  //   y: 0,
-  //   dx: 0,
-  //   dy: 0,
-  //   px: 0,
-  //   py: 0,
-  // }
-  // context: ...,
+  context: {
+    // Sets the rendered position of the DOM el, in <body>
+    // coord space
+    x: 0,
+    y: 0,
+    // Sets the relative rendered offset from X/Y
+    dx: 0,
+    dy: 0,
+    // The remembered point where we clicked the box; used to calculate the 
+    // delta/offset... in <body> coord space
+    px: 0,
+    py: 0,
+  },
   states: {
     idle: {
       on: {
         mousedown: {
-          // Assign the point
-          // ...
           target: 'dragging',
+          actions: ['setPoint'],
         },
       },
     },
     dragging: {
       on: {
         mousemove: {
-          // Assign the delta
-          // ...
-          // (no target!)
+          target: 'dragging',
+          internal: true,
+          actions: ['setDelta' ],
         },
         mouseup: {
-          // Assign the position
           target: 'idle',
+          actions: ['setPosition']
         },
       },
     },
   },
+},
+{
+  actions: {
+    setPoint: assign({
+      px: (ctx, ev) => ev.clientX,
+      py: (ctx, ev) => ev.clientY,
+    }),
+    setDelta: assign({
+      dx: (ctx, ev) => ev.clientX - ctx.px,
+      dy: (ctx, ev) => ev.clientY - ctx.py,
+    }),
+    setPosition: assign({
+      x: (ctx, ev) => ctx.x + ctx.dx,
+      y: (ctx, ev) => ctx.y + ctx.dy,
+      dx: 0,
+      dy: 0,
+      px: 0,
+      py: 0,
+    }),
+  }
 });
 
 const service = interpret(machine);
@@ -49,8 +72,8 @@ service.onTransition((state) => {
 
     elBox.dataset.state = state.value;
 
-    elBox.style.setProperty('--dx', state.context.dx);
-    elBox.style.setProperty('--dy', state.context.dy);
+//    elBox.style.setProperty('--dx', state.context.dx);
+ //   elBox.style.setProperty('--dy', state.context.dy);
     elBox.style.setProperty('--x', state.context.x);
     elBox.style.setProperty('--y', state.context.y);
   }
@@ -58,7 +81,7 @@ service.onTransition((state) => {
 
 service.start();
 
-// Add event listeners for:
-// - mousedown on elBox
-// - mousemove on elBody
-// - mouseup on elBody
+elBox.addEventListener('mousedown', service.send);
+elBody.addEventListener('mousemove', service.send);
+elBody.addEventListener('mouseup', service.send);
+
