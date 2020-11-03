69c62a39b9c136befb9ab70fcd3a496c55e2c280
Raphael Traviss <raphael@skyleafdesign.com>
2020-11-03 10:58:30 -0600

    Exercise 10 remember light/dark state

--- src/10/index.js
+++ src/10/index.js
@@ -10,15 +10,20 @@ const displayMachine = createMachine({
   states: {
     hidden: {
       on: {
-        TURN_ON: 'visible',
+        TURN_ON: 'visible.remembered',
       },
     },
     visible: {
-      // Add hierarchical states for light/dark mode.
-      // ...
-
-      // Then, add a history state that remembers which mode we used.
-      // ...
+      initial: 'light',
+      states: {
+        light: {
+          on: { SWITCH: 'dark' },
+        },
+        dark: {
+          on: { SWITCH: 'light' },
+        },
+        remembered: { type: 'history' },
+      },
       on: {
         TURN_OFF: 'hidden',
       },
@@ -31,11 +36,6 @@ const displayService = interpret(displayMachine)
     elApp.dataset.state = state.toStrings().join(' ');
   })
   .start();
-
-// Add event listeners for:
-// - clicking elOnButton (TURN_ON)
-// - clicking elOffButton (TURN_OFF)
-// - clicking elModeButton (SWITCH)
 
 elOnButton.addEventListener('click', () => {
   displayService.send('TURN_ON');
